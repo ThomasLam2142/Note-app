c61f85f1603c99724061eb01936bdca842a60f25
Thomas Lam <thomaslam2142@gmail.com>
2023-09-14 19:01:18 -0400

    Delete Button logic implemented

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const createButton = document.querySelector(".createButton");
 const allNotesContainer = document.querySelector(".allNoteContainer");
+let noteidCounter = 1;
 
 createButton.addEventListener("click", () => {
+    //iterate note counter
+    noteidCounter += 1;
+
     // Create a new div element
     const newNote = document.createElement("div");
-    
+    const noteID = noteidCounter;
+
+    //Give each note a ID
+    newNote.id ="note" + noteID;
+    console.log(newNote.id)
 
     // Set the content of the new note (you can customize this)
     newNote.textContent = "This is a new note.";
@@ -21,6 +29,15 @@ createButton.addEventListener("click", () => {
     newDelete.textContent = "Delete Note";
 
     newDelete.classList.add("deleteButton");
+    
+    newDelete.onclick = function() {
+        // Find the parent note of the clicked delete button and remove it
+        const parentNote = this.parentElement;
+        console.log("removed " + newNote.id)
+        parentNote.remove();
+    };
 
     newNote.appendChild(newDelete);
 });
+
+
